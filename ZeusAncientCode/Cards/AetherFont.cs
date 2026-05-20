3e5982001df910cddddc78df8e5ee2d7b5c101f8
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-05-20 22:42:00 +0200

    Add 'Aether Font' card.

---
+++ ZeusAncientCode/Cards/AetherFont.cs
@@ -0,0 +1,28 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace ZeusAncient.ZeusAncientCode.Cards;
+
+[Pool(typeof(EventCardPool))]
+public class AetherFont() : ZeusAncientCard(0, CardType.Skill, CardRarity.Token, TargetType.Self)
+{
+    public override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+        [CardKeyword.Exhaust, CardKeyword.Retain];
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips => [EnergyHoverTip];
+
+
+    public override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.IntValue, Owner);
+    }
+
+    public override void OnUpgrade() => DynamicVars.Energy.UpgradeValueBy(1);
+}
