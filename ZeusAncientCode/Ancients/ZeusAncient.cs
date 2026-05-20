404e36a699c56e819d3eec23857d8051a988f7a7
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-05-20 21:25:18 +0200

    Add Ancient class. Add art for: Ancient Background, Map Icon, Run History Icon and Mod Image.

---
+++ ZeusAncientCode/Ancients/ZeusAncient.cs
@@ -0,0 +1,40 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using ZeusAncient.ZeusAncientCode.Extensions;
+
+namespace ZeusAncient.ZeusAncientCode.Ancients;
+
+[Pool(typeof(AncientEventModel))]
+public class ZeusAncient : CustomAncientModel
+{
+    public override string CustomScenePath => "zeus.tscn".AncientImagePath();
+    public override string CustomMapIconPath => "map_icon.png".AncientImagePath();
+    public override string CustomMapIconOutlinePath => "map_icon_outline.png".AncientImagePath();
+    public override string CustomRunHistoryIconPath => "run_history_icon.png".AncientImagePath();
+    public override string CustomRunHistoryIconOutlinePath => "run_history_icon_outline.png".AncientImagePath();
+
+    protected override OptionPools MakeOptionPools
+    {
+        get
+        {
+            List<AncientOption> relics =
+            [
+                AncientOption<Vajra>(),
+                AncientOption<OddlySmoothStone>(),
+                AncientOption<DataDisk>()
+            ];
+
+            return new OptionPools(
+                MakePool(relics.ToArray())
+            );
+        }
+    }
+
+    public override bool IsValidForAct(ActModel act)
+    {
+        return act.ActNumber() == 2;
+    }
+}
---
+++ ZeusAncientCode/Patches/ZeusAncient_AdjustLayout_Patch.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Events;
+
+namespace ZeusAncient.ZeusAncientCode.Patches;
+
+[HarmonyPatch(typeof(NAncientEventLayout), "SetDialogueLineAndAnimate")]
+public static class ZeusAncient_AdjustLayout_Patch
+{
+    private const float XOffset = 170f;
+    private const float YOffset = 30f;
+    private const float ScaleAmount = 0.925f;
+    private const float OriginalSpacing = 10f;
+
+    private static readonly ConditionalWeakTable<NAncientEventLayout, Box> State = new();
+
+    private static bool IsTargetEvent(NAncientEventLayout layout)
+    {
+        var ancientEvent = Traverse.Create(layout)
+            .Field("_ancientEvent")
+            .GetValue<AncientEventModel>();
+
+        return ancientEvent.Id == ModelDb.GetId<Ancients.ZeusAncient>();
+    }
+
+    static void Prefix(NAncientEventLayout __instance)
+    {
+        if (!IsTargetEvent(__instance))
+            return;
+
+        var t = Traverse.Create(__instance);
+
+        var content = t.Field("_content").GetValue<VBoxContainer>();
+        var contentContainer = t.Field("_contentContainer").GetValue<Control>();
+
+        if (content == null || contentContainer == null)
+            return;
+
+        if (!State.TryGetValue(__instance, out var state))
+        {
+            state = new Box(content.Position.X, contentContainer.Size.X);
+            State.Add(__instance, state);
+        }
+
+        content.Position = new Vector2(
+            state.BaseContentX + XOffset,
+            content.Position.Y
+        );
+
+        content.Scale = new Vector2(ScaleAmount, ScaleAmount);
+
+        contentContainer.ClipContents = false;
+
+        float extraWidth = Mathf.Abs(XOffset) * 2f;
+
+        contentContainer.Size = new Vector2(
+            state.BaseContainerWidth + extraWidth,
+            contentContainer.Size.Y
+        );
+    }
+
+    private static float GetSpacingForEvent(NAncientEventLayout layout, float originalSpacing)
+    {
+        if (!IsTargetEvent(layout))
+            return originalSpacing;
+
+        return originalSpacing - YOffset;
+    }
+
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var code in instructions)
+        {
+            if (code.opcode == OpCodes.Ldc_R4 &&
+                code.operand is float f &&
+                f == OriginalSpacing)
+            {
+                yield return new CodeInstruction(OpCodes.Ldarg_0);
+                yield return new CodeInstruction(OpCodes.Ldc_R4, OriginalSpacing);
+                yield return CodeInstruction.Call(
+                    typeof(ZeusAncient_AdjustLayout_Patch),
+                    nameof(GetSpacingForEvent)
+                );
+            }
+            else
+            {
+                yield return code;
+            }
+        }
+    }
+
+    private sealed class Box
+    {
+        public readonly float BaseContainerWidth;
+        public readonly float BaseContentX;
+
+        public Box(float x, float width)
+        {
+            BaseContentX = x;
+            BaseContainerWidth = width;
+        }
+    }
+}
