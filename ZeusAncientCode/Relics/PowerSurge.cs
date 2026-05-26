9df0adfc7b97e5fc9c3fdb27ae29f37852367972
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-05-26 18:26:26 +0200

    Add counter to 'Power Surge'.

--- ZeusAncientCode/Relics/PowerSurge.cs
+++ ZeusAncientCode/Relics/PowerSurge.cs
@@ -1,10 +1,12 @@
 ﻿using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Rooms;
 using MegaCrit.Sts2.Core.ValueProps;
 using ZeusAncient.ZeusAncientCode.Utils;
 
@@ -14,6 +16,10 @@ namespace ZeusAncient.ZeusAncientCode.Relics;
 public class PowerSurge : ZeusAncientRelic
 {
     public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    public override bool ShowCounter => CombatManager.Instance.IsInProgress;
+
+    public override int DisplayAmount => (int)CalculateFinalDamage();
 
     public override IEnumerable<DynamicVar> CanonicalVars =>
     [
@@ -25,11 +31,36 @@ public class PowerSurge : ZeusAncientRelic
         CombatSide side,
         IEnumerable<Creature> participants)
     {
-        Decimal finalDamage = DynamicVars.Damage.BaseValue * Owner.PlayerCombatState?.TurnNumber ?? 0;
+        Decimal finalDamage = CalculateFinalDamage();
         if (!participants.Contains(Owner.Creature) || Owner.PlayerCombatState?.Energy <= 0 || finalDamage <= 0)
             return;
 
         await ZeusUtils.DealLightningDamageToAll(choiceContext, Owner.Creature,
             Owner.Creature.CombatState?.HittableEnemies ?? [], finalDamage);
+    }
+
+    public override Task BeforeHandDraw(
+        Player player,
+        PlayerChoiceContext choiceContext,
+        ICombatState combatState)
+    {
+        if (player != Owner)
+        {
+            return Task.CompletedTask;
+        }
+
+        InvokeDisplayAmountChanged();
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterCombatEnd(CombatRoom _)
+    {
+        InvokeDisplayAmountChanged();
+        return Task.CompletedTask;
+    }
+
+    private decimal CalculateFinalDamage()
+    {
+        return DynamicVars.Damage.BaseValue * Owner.PlayerCombatState?.TurnNumber ?? 0;
     }
 }
